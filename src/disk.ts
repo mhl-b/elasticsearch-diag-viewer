108cb8af5a8b4b95eccc5435613c0ae605f20f70
Mikhail Berezovskiy <mikhail@elastic.co>
2024-06-03 10:55:36 -0700

    indent

--- src/disk.ts
+++ src/disk.ts
@@ -3,35 +3,35 @@ const strPattern = new RegExp(/^\s*(?<size>\d+(?:\.\d+)?) *(?<unit>[kmgtpKMGTP]?
 export type SizeBytes = number;
 
 export function parseDiskSize(diskSize: string): SizeBytes {
-	//@ts-ignore
-	let { size, unit } = strPattern.exec(diskSize).groups;
-	size = Number(size);
-	switch (unit) {
-		case 'b':
-			return size;
-		case 'kb':
-			return size * 1024;
-		case 'mb':
-			return size * 1024 ** 2;
-		case 'gb':
-			return size * 1024 ** 3;
-		case 'tb':
-			return size * 1024 ** 4;
-		default:
-			throw new Error(`cannot parse unit: ${unit} from ${diskSize}`);
-	}
+  //@ts-ignore
+  let { size, unit } = strPattern.exec(diskSize).groups;
+  size = Number(size);
+  switch (unit) {
+    case 'b':
+      return size;
+    case 'kb':
+      return size * 1024;
+    case 'mb':
+      return size * 1024 ** 2;
+    case 'gb':
+      return size * 1024 ** 3;
+    case 'tb':
+      return size * 1024 ** 4;
+    default:
+      throw new Error(`cannot parse unit: ${unit} from ${diskSize}`);
+  }
 }
 
 export function displayDiskSize(bytes: SizeBytes): string {
-	if (bytes < 1024) {
-		return `${bytes}b`
-	} else if (bytes < 1024 ** 2) {
-		return `${(bytes / 1024).toFixed(2)}kb`
-	} else if (bytes < 1024 ** 3) {
-		return `${(bytes / 1024 ** 2).toFixed(2)}mb`
-	} else if (bytes < 1024 ** 4) {
-		return `${(bytes / 1024 ** 3).toFixed(2)}gb`
-	} else {
-		return `${(bytes / 1024 ** 4).toFixed(2)}tb`
-	}
+  if (bytes < 1024) {
+    return `${bytes}b`
+  } else if (bytes < 1024 ** 2) {
+    return `${(bytes / 1024).toFixed(2)}kb`
+  } else if (bytes < 1024 ** 3) {
+    return `${(bytes / 1024 ** 2).toFixed(2)}mb`
+  } else if (bytes < 1024 ** 4) {
+    return `${(bytes / 1024 ** 3).toFixed(2)}gb`
+  } else {
+    return `${(bytes / 1024 ** 4).toFixed(2)}tb`
+  }
 }
